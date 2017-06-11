a38f89ae2b43593076d89690a6c0cd6ede7c301d
MichaelJiang <dita51519961@gmail.com>
2017-06-11 12:52:25 +0800

    2017-06-11
    成功存储MQTT回传车辆信息

--- util/ReceiverDataFromMqtt.js
+++ util/ReceiverDataFromMqtt.js
@@ -1,54 +1,82 @@
 /**
- * Created by wushuran on 17-6-6.
+ * Created by michaeljiang on 17-6-6.
+ */
+/** MongoDb连接用变量 **/
+/**
+ * Created by michaeljiang on 17-6-6.
  */
 
-/** MongoDb连接用变量 **/
-var mqtt  = require('mqtt');
-var uuid = require('uuid');
 
-var client  = mqtt.connect('mqtt://10.66.15.225',{
-    username:'team6',
-    password:'123456',
-    clientId:uuid.v4()
-});
+module.exports = function startReceiverData(){
+    /** MongoDb连接用变量 **/
+    var mongoose = require('mongoose');    //引用mongoose模块
+    var db = mongoose.createConnection('localhost','ubinetcar'); //创建一个数据库连接
 
+    var CarSchema = new mongoose.Schema({
+        carID:String,   //定义一个属性name，类型为String
+        carType:String,
+        own:mongoose.Schema.Types.Mixed,
+    });
+    var DrivingRecordSchema = new mongoose.Schema({
+        carID:mongoose.Schema.ObjectId,
+        engineCoolantTemperature:Number,
+        engineSpeed:Number,
+        vehicleSpeed:Number,
+        mailboxAirTemperature:Number,
+        mailboxLevel:Number,
+        engineOilTemperature:Number,
+        engineOilConsumptionRate:Number,
+        location:mongoose.Schema.Types.Mixed
+    })
+    var CarModel = db.model('Car',CarSchema);
+    var DrivingRecordModel = db.model('DrivingRecord',DrivingRecordSchema);
 
-/** 测试用数据 **/
-var testJson = {
-    carID:'31401097',
-    engineCoolantTemperature:233,
-    engineSpeed:233,
-    vehicleSpeed:233,
-    mailboxAirTemperature:233,
-    mailboxLevel:233,
-    engineOilTemperature:233,
-    engineOilConsumptionRate:233,
-    location:{
-        'geo': [
-            22,//geo:lng, lat
-            22
-        ],
-        'country': "1",
-        'postcode': "1",
-        'state': "1",
-        'suburb': "1",
-        'street2': "1",
-        'street1': "11",
-        'name': "11",
-        'number': "1"
-    }
+    /** Mqtt连接用变量 **/
+    var mqtt  = require('mqtt');
+    var uuid = require('uuid');
+
+    /** 数据库链接操作 **/
+    db.on('error',console.error.bind(console,'连接错误:'));
+    db.once('open',function(){
+        console.log("数据库连接成功");
+    });
+
+    var client  = mqtt.connect('mqtt://10.66.15.225',{
+        username:'michaeljiang',
+        password:'123456',
+        clientId:uuid.v4()
+    });
+    client.on('connect', function () {
+        console.log('connected.....');
+        client.subscribe('ubinetcar_driving_record');
+        client.subscribe('ubinetcar_message');
+    });
+
+    client.on('message', function (topic, message) {
+        try{
+            var message = JSON.parse(message.toString());
+            //成功转换字符串
+            if(topic == 'ubinetcar_driving_record'){
+                CarModel.findOne({carID:message.carID},function (err,car) {
+                    if(car == null){
+                        client.publish('ubinetcar_message', JSON.stringify({code:-1,message:"NoThisCar"}));
+                        console.log(JSON.stringify({code:-1,message:"NoThisCar"}));
+                        return;
+                    }
+                    message.carID = car._id;
+                    var DrivingRecordEnity = new DrivingRecordModel(message);
+                    DrivingRecordEnity.save();
+                    console.log(1);
+                }).then(function () {
+                    console.log(JSON.stringify({code:0,message:"Success"}));
+                })
+            }
+            if(topic=='ubinetcar_message'){
+                console.log(message);
+            }
+        }
+        catch (err){
+            console.log(err)
+        }
+    });
 }
-var sendString = JSON.stringify(testJson);
-
-
-client.on('connect', function () {
-    console.log('connected.....');
-    client.subscribe('test');
-    client.publish('test', JSON.stringify({code:0,message:"LinkSuccess"}));
-});
-
-client.on('message', function (topic, message) {
-    // message is Buffer
-    console.log("topic  : "+topic);
-    console.log("message: "+message);
-});
