01a5bb1b4c0a289acf696c39ec6fcb77351ae86a
MichaelJiang <dita51519961@gmail.com>
2017-06-14 23:52:56 +0800

    2017-6-14
    将Html页面转换成pug的模板，并且填写相关的加载路由

--- routes/index.js
+++ routes/index.js
@@ -36,7 +36,9 @@ exports = module.exports = function (app) {
 	// Views
 	app.get('/', routes.views.index);
 	app.all('/contact', routes.views.contact);
-
+	app.all('/login',routes.views.login);
+	app.all('/register',routes.views.register);
+	app.all('/dataRecord',routes.views.dataRecord);
 	// NOTE: To protect a route so that only admins can see it, use the requireUser middleware:
 	// app.get('/protected', middleware.requireUser, routes.views.protected);
 
